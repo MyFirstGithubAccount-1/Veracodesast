0158af3dd6969742f608c9faeb51bb12bcf67aae
Sugayuv1 <107237480+Sugayuv1@users.noreply.github.com>
2022-09-30 11:08:53 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,19 @@
 <?php
-include("../../lib/client/JsonRpcClient.php");
-include("../../lib/client/AuthenticatedJsonRpcClient.php");
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	include("../../lib/server/JsonRpcServer.php");
+	include("MathServiceImpl.php");
+    session_start();
+	/*
+	$server = new JsonRpcServer(file_get_contents("php://input"));
 
-$listOfCalls = array();
+	$server->addService(new MathServiceImpl());
+	$server->processingRequests();
 
-array_push($listOfCalls,new RpcRequest("add",array(33,77)));
-array_push($listOfCalls,new RpcRequest("divide",array(44,11),true));
-array_push($listOfCalls,new RpcRequest("subtract",array(2,12.3)));
-array_push($listOfCalls,new RpcRequest("invalidateSession"));
-array_push($listOfCalls,new RpcRequest("something"));
+	*/
+	$server = new AuthenticatorJsonRpcServer(file_get_contents("php://input"));
 
-$client = new AuthenticatedJsonRpcClient('http://localhost/jsonrpc/sample/server/','test','1234');
-//$client = new JsonRpcClient('http://localhost/jsonrpc/sample/server/');
-/*
-echo '<pre>';
-var_dump($client->subtract(2,9.2));
-echo '</pre>';
-
-echo '<pre>';
-var_dump($client->callBatch($listOfCalls));
-echo '</pre>';
-*/
-/*
- */
-echo '<pre>';
-var_dump($client->subtract(2,9.2));
-var_dump($client->something());
-var_dump($client->addPerson('asd'));
-var_dump($client->call(new RpcRequest("invalidateSession",null)));
-var_dump($client->something());
-echo '</pre>';
-
+	$server->addService(new MathServiceImpl());
+    $server->addService(new PersonServiceImpl());
+	$server->processingRequests();
+}
 ?>
